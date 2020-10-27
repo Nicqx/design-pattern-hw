d25da54913023c425d67d282fdeebadfc106a596
mparrag <miklos_parrag@epam.com>
2020-10-27 19:26:22 +0100

    overall typo corrected

--- src/com/company/observer/NumberCounterListener.java
+++ src/com/company/observer/NumberCounterListener.java
@@ -14,19 +14,19 @@ public class NumberCounterListener implements EventListener {
     @Override
     public void update(String eventType, File file) {
         try {
-            int overalCount = 0;
+            int overallCount = 0;
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] words = line.split(" ");
                 for (String word : words) {
                     if (word.matches("-?\\d+")) {
-                        overalCount++;
+                        overallCount++;
                     }
                 }
             }
             scanner.close();
-            System.out.println("The number of numbers: " + overalCount + " in this file: " + file.getName());
+            System.out.println("The number of numbers: " + overallCount + " in this file: " + file.getName());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
--- src/com/company/observer/WordCounterListener.java
+++ src/com/company/observer/WordCounterListener.java
@@ -14,15 +14,15 @@ public class WordCounterListener implements EventListener {
     @Override
     public void update(String eventType, File file) {
         try {
-            int overalCount = 0;
+            int overallCount = 0;
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()){
                 String line = scanner.nextLine();
                 String[] words = line.split(" ");
-                overalCount += words.length;
+                overallCount += words.length;
             }
             scanner.close();
-            System.out.println("The number of words: " + overalCount + " in this file: " + file.getName());
+            System.out.println("The number of words: " + overallCount + " in this file: " + file.getName());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
