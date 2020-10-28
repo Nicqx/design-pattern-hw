3d608cf4afb3acf5c3d14d393dacde05b170443a
mparrag <miklos_parrag@epam.com>
2020-10-28 10:12:01 +0100

    eventmanager s variable rename

--- src/com/company/observer/FileOpener.java
+++ src/com/company/observer/FileOpener.java
@@ -4,14 +4,13 @@ import java.io.File;
 
 public class FileOpener {
     public EventManager events;
-    private File file;
 
-    public FileOpener(){
+    public FileOpener() {
         this.events = new EventManager("word counter", "number counter", "longest word", "reverse word");
     }
 
-    public void openFile(String filePath){
-        this.file = new File(filePath);
+    public void openFile(String filePath) {
+        File file = new File(filePath);
         events.notify("word counter", file);
         events.notify("number counter", file);
         events.notify("longest word", file);
