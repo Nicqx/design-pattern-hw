8f92909ae571075638ed881f855bc9f8133df975
mparrag <miklos_parrag@epam.com>
2020-10-28 10:13:39 +0100

    local prefix remove

--- src/com/company/observer/LongestWordKeeperListener.java
+++ src/com/company/observer/LongestWordKeeperListener.java
@@ -8,20 +8,20 @@ public class LongestWordKeeperListener implements EventListener {
 
     @Override
     public void update(String eventType, File file) {
-        String localLongestWord = "";
-        int localLongestCount = 0;
+        String longestWord = "";
+        int longestCount = 0;
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] words = line.split(" ");
                 for (String word : words) {
-                    if (word.length() > localLongestCount) {
-                        localLongestCount = word.length();
-                        localLongestWord = word;
+                    if (word.length() > longestCount) {
+                        longestCount = word.length();
+                        longestWord = word;
                     }
                 }
             }
-            System.out.println("The longest word length is: " + localLongestCount + " and the word is: '" + localLongestWord + "' in this file: " + file.getName());
+            System.out.println("The longest word length is: " + longestCount + " and the word is: '" + longestWord + "' in this file: " + file.getName());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
