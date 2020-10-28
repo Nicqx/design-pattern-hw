f5f1ac10fc0ad4c5d09d3d8a99fb9cc01bafc503
mparrag <miklos_parrag@epam.com>
2020-10-28 10:49:05 +0100

    singleton double-checked locking

--- src/com/company/singleton/Superman.java
+++ src/com/company/singleton/Superman.java
@@ -1,16 +1,18 @@
 package com.company.singleton;
 
 public class Superman {
-    private static Superman instance;
+    private static volatile Superman instance;
     public String say;
 
     private Superman(String say) {
         this.say = say;
     }
 
-    public static synchronized Superman getInstance(String say) {
+    public static Superman getInstance(String say) {
         if (instance == null) {
-            instance = new Superman(say);
+            synchronized (Superman.class) {
+                instance = new Superman(say);
+            }
         }
         return instance;
     }
